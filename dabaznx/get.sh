618146535f8a802e739408d86e69a41f0c680e0d
Manuel Sanchez del Rio <srio@esrf.eu>
2020-12-09 09:36:01 +0100

    clean

--- dabaznx/get.sh
+++
@@ -1,4 +0,0 @@
-wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/AtomicConstants.dat
-wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/f1f2_EPDL97.dat
-wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/Crystals.dat
-
