b9900c9088c7fede0ac392818584b86a96b84ca8
Manuel Sanchez del Rio <srio@esrf.eu>
2020-12-09 08:42:15 +0100

    first draft

---
+++ dabaznx/convert.py
@@ -0,0 +1,40 @@
+from silx.io.specfile import SpecFile
+import h5py
+
+def convert(filename):
+
+    sf = SpecFile(filename+".dat")
+
+    s1 = sf[0]
+
+
+    f = h5py.File(filename+'.h5', 'w')
+    f["header"] = str(s1.header)
+
+    for index in range(len(sf)):
+        s1 = sf[index]
+        name = s1.scan_header_dict["S"]
+        f1 = f.create_group(name)
+
+        for i,label in enumerate(s1.labels):
+            f1[label] = s1.data[i,:]
+
+        f2 = f1.create_group("metadata")
+        for key in s1.scan_header_dict.keys():
+            f2[key] = s1.scan_header_dict[key]
+
+
+    f.close()
+    print("File written: ", filename+'.h5')
+
+if __name__ == "__main__":
+
+    convert("AtomicConstants")
+    convert("f1f2_EPDL97")
+    convert("Crystals")
+
+
+
+
+
+
---
+++ dabaznx/get.sh
@@ -0,0 +1,4 @@
+wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/AtomicConstants.dat
+wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/f1f2_EPDL97.dat
+wget http://ftp.esrf.eu/pub/scisoft/DabaxFiles/Crystals.dat
+
